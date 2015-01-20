9f564484a1e792febfb950d4cc1f3be7e0a21131
Ricardo Sánchez Amil <ricardosanchezamil@hotmail.com>
2015-01-19 19:33:29 -0600

    Update LenguajesAmil.java

--- src/lenguajes/amil/LenguajesAmil.java
+++ src/lenguajes/amil/LenguajesAmil.java
@@ -15,7 +15,7 @@ public class LenguajesAmil {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-          System.out.println("Mi primer programa de Java II");
+          System.out.println("Línea modificada");
     }
     
 }
